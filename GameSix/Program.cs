eea8fc5294a28555f976f2101bb87bbb1571ae70
David Mendes <david.f.mendes@proton.me>
2023-04-20 10:55:48 +0100

    Dynamically creates an inputted number of enemies, sets their name and returns each created foe's name on a new line

--- GameSix/Program.cs
+++ GameSix/Program.cs
@@ -6,7 +6,22 @@ namespace GameSix
     {
         private static void Main(string[] args)
         {
-            
+            Console.WriteLine("Insert number of enemies");
+            int enemyNum = Convert.ToInt16(Console.ReadLine());
+            Foe[] enemyArray = new Foe[enemyNum];
+
+            for (int i = 0; i < enemyNum; i++)
+            {
+                Console.WriteLine($"Creating enemy nº{i + 1}\nEnter it's name");
+                string newEnemyName = Console.ReadLine()!;
+
+                enemyArray[i] = new Foe(newEnemyName);
+            }
+
+            foreach (var enemy in enemyArray)
+            {
+                Console.WriteLine(enemy.GetName());
+            }
         }
     } 
 }
