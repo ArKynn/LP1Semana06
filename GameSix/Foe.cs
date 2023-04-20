e1ecf4fd8cb1eddee8021961ebd6dd72ab65a769
David Mendes <david.f.mendes@proton.me>
2023-04-20 10:09:31 +0100

    Added GetName and TakeDamage methods to foe class

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -3,12 +3,29 @@
 public class Foe
 {
     private string Name { get; }
-    private int Health { get; set; }
-    private int Shield { get; set; }
+    private float Health { get; set; }
+    private float Shield { get; set; }
     public Foe(string name)
     {
         this.Name = name;
         Health = 100;
         Shield = 0;
+    }
+
+    public string GetName()
+    {
+        return this.Name;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Shield -= damage;
+        if (Shield < 0)
+        {
+            float remainingDamage = -Shield;
+            Shield = 0;
+            Health -= remainingDamage;
+            if (Health < 0) Health = 0;
+        }
     }
 }
