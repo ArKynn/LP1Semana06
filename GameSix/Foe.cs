69a45ed1a80a01d3363e2226fd144fc50bcd69c5
David Mendes <david.f.mendes@proton.me>
2023-04-20 10:05:00 +0100

    Added Foe class constructor

--- GameSix/Foe.cs
+++ GameSix/Foe.cs
@@ -2,5 +2,13 @@
 
 public class Foe
 {
-    
+    private string Name { get; }
+    private int Health { get; set; }
+    private int Shield { get; set; }
+    public Foe(string name)
+    {
+        this.Name = name;
+        Health = 100;
+        Shield = 0;
+    }
 }
